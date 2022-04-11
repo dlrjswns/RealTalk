9e924d6ad0abc14d7f100da62d8a47e450ce8e89
dlrjswns <leekyunjun@gmail.com>
2022-04-11 09:03:08 +0900

    Initial Commit

---
+++ RealTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTalk
+//
+//  Created by 이건준 on 2022/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
